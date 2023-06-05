b229cf7e79dea65a9cc469caf6373409a709c39e
Tianhao-Qi <qiy.jia@foxmail.com>
2023-06-05 18:42:21 +0000

    fix bugs in test.py

--- tools/test.py
+++ tools/test.py
@@ -179,10 +179,7 @@ def main():
         model = fuse_conv_bn(model)
     # old versions did not save class info in checkpoints, this walkaround is
     # for backward compatibility
-    if 'CLASSES' in checkpoint['meta']:
-        model.CLASSES = checkpoint['meta']['CLASSES']
-    else:
-        model.CLASSES = dataset.CLASSES
+    model.CLASSES = dataset.CLASSES
 
     if not distributed:
         model = MMDataParallel(model, device_ids=[0])
