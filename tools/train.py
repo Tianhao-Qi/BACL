c990daeb9cfe08275e5f8a8ef800e98627bc95a3
Tianhao-Qi <qiy.jia@foxmail.com>
2023-06-06 04:49:41 +0000

    fix bugs in train.py and upload resources

--- tools/train.py
+++ tools/train.py
@@ -81,6 +81,10 @@ def parse_args():
         default='none',
         help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument(
+        '--auto-scale-lr',
+        action='store_true',
+        help='enable automatically scaling LR.')
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
